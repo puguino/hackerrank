dbccaec978aab9e17d4bf939eadabda33463f28e
puguino <puguinopugu@outlook.com>
2019-02-27 09:45:53 +0700

    new

---
+++ Day1_DataTypes.py
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+print(d + d2)
+print(s + s2)
